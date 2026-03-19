cf78236088107310e0eeaf90fc5e071a5b3716cd
vimavill20 <107956197+vimavill20@users.noreply.github.com>
2026-03-19 17:03:18 -0300

    New meshes added

---
+++ Malhas/GEO/SingleFracture90deg.geo
@@ -0,0 +1,29 @@
+cl__1 = 35;
+Point(1) = {30, 8, 0, cl__1};
+Point(5) = {30, 57, 0, cl__1};
+Point(12) = {0, 0, 0, cl__1};
+Point(13) = {0, 77, 0, cl__1};
+Point(14) = {66, 77, 0, cl__1};
+Point(15) = {66, 0, 0, cl__1};
+
+
+Line(12) = {12, 13};
+Line(13) = {13, 14};
+Line(14) = {14, 15};
+Line(15) = {15, 12};
+Line(16) = {5,1};
+Line(17) = {1,5};
+//Curve Loop(11) = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//Plane Surface(11) = {11};
+Curve Loop(11) = {16,17};
+Plane Surface(11) = {11};
+Physical Curve("SmallFract",300)={16,17};
+
+//Curve Loop(1000) = {1, 2, 3, 4, 5, 6, 7, 8, 9, -15, -14, -13, -12};
+Curve Loop(1000) = { 16,17, -15, -14, -13, -12};
+//Curve Loop(1000) = {  -15, -14, -13, -12};
+
+Plane Surface(1000) = {1000};
+Physical Surface("k11", 1) = {1000};
+//+
+Physical Curve("inlet", 2) = {14};
--- Targets/SimulacaoPconstHdiv/main.cpp
+++ Targets/SimulacaoPconstHdiv/main.cpp
@@ -201,9 +201,11 @@ void Hdiv_Fract(){
     dim_name_and_physical_tagCoarse[1]["outlet"] = EbcOutletId;
     dim_name_and_physical_tagCoarse[1]["noflux"] = EbcNoFlux;
     dim_name_and_physical_tagCoarse[1]["SmallFract"] = 6;
+    dim_name_and_physical_tagCoarse[2]["Vugs"] = 7;
+
 
     //std::string filename="/Users/victorvillegassalabarria/python-test/testskelSLICE77SP.msh";
-    //std::string filename="/Users/victorvillegassalabarria/Documents/Github/Vugs/FastMesh.msh";
+    std::string filename="/Users/victorvillegassalabarria/Documents/Github/Vugs/Malhas/FractureVug.msh";
     //std::string filename="/home/marina/programming/Stokes-Darcy-Research/VUGS/FastMesh.msh";
     //std::string filename="/Users/victorvillegassalabarria/Documents/Github/Vugs/FewVugsMesh.msh";
     //std::string filename="/Users/victorvillegassalabarria/Downloads/MallaTriangles123.msh";
@@ -215,7 +217,7 @@ void Hdiv_Fract(){
     // std::ofstream file24("Test_cmeshFlux.txt");
     // std::ofstream file25("Test_cmeshMulti.txt");
     //std::string filename="/Users/victorvillegassalabarria/Downloads/SingleFracture1.msh";
-    std::string filename="/Users/victorvillegassalabarria/Downloads/FewFractures.msh";
+    //std::string filename="/Users/victorvillegassalabarria/Downloads/FewFractures.msh";
 
     //std::string filename="/home/marina/programming/Stokes-Darcy-Research/VUGS/SingleFracture.msh";
     std::ofstream file20("TestGeoMesh2D.vtk");
